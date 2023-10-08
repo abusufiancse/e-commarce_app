d3ff11a0c3a3ffab73caeaf76276b836c8a88d8a
abusufiancse <abu15-2532@diu.edu.bd>
2023-10-08 20:40:47 +0600

    upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# ecomarce_app
+
+
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// Add this import statementimport 'package:ecomarce_app/home_screen.dart';
+import 'package:ecomarce_app/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: Colors.transparent,
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
